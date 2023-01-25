0f548a4f917ecfe9aab942b77b7a67dd5641852d
kusainovratmir3@gmail.com <ratmirkusainov4622@gmail.com>
2023-01-25 19:48:40 +0400

    add navigation bar for each page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { Navigation } from "./components/common/Navigation";
 import { Greeting } from "./components/sections/Greeting";
 
 export const App = () => {
-  return <Greeting />
+  return <>
+    <Greeting />
+  
+    <Navigation />
+  </>
 }
---
+++ src/components/common/Navigation.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+export const Navigation = () => {
+    return <div className={`fixed list-[circle] list-inside top-1/2 right-[20px] -translate-y-1/2`}>
+        {
+            [1,2,3,4,5,6].map((num, idx) => <div key={idx} className={`
+                group
+                flex items-center mt-3 font-network text-white
+                cursor-pointer
+            `}>
+                <span className={`min-w-[30px] opacity-0 text-center group-hover:opacity-100`}>{num}</span>
+                <div className={'w-[15px] h-[15px] pl-2 bg-[#F3cc30] border-solid border-2 border-black rounded-full'}></div>
+            </div>)
+        }
+    </div>
+}
