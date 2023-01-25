cf9d04f751f9ab256459f20fb478b28b97d5cbf0
kusainovratmir3@gmail.com <ratmirkusainov4622@gmail.com>
2023-01-25 18:15:40 +0400

    remve uneccessary three.js intergration and add sroll animation

--- src/components/sections/Greeting.tsx
+++ src/components/sections/Greeting.tsx
@@ -1,77 +1,67 @@
 import React, { useState, useEffect } from "react";
-import * as THREE from 'three';
-import mirror from '../../../public/sections/air.png';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-
-function runSphere() {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-    const renderer = new THREE.WebGLRenderer({
-        alpha: true,
-        canvas: document.getElementById('sphere')
-    });
-
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(0x000000, 0);
-
-    camera.position.setZ(30);
-
-    renderer.render(scene, camera);
-
-    const geometry = new THREE.SphereGeometry( 15, 32, 16 );
-    const texture = new THREE.TextureLoader();
-    const material = new THREE.MeshPhysicalMaterial( { 
-        map: texture.load(mirror),
-        cleacoatRoughness: 1,
-        metalness: 0.9,
-        ior: 1,
-        roughness: 0.5,
-        // color: 0x8418ca,
-        normalMap: texture.load(mirror),
-        normalScale: new THREE.Vector2(0.15, 0.15, 0.15)
-    });
-
-    const light = new THREE.PointLight("#392c33", 20);
-    light.position.set(30,30,30);
-    scene.add(light);
-    
-    const sphere = new THREE.Mesh( geometry, material );
-    scene.add( sphere );
-
-    const controls = new OrbitControls(camera, renderer.domElement);
-    function animate() {
-        requestAnimationFrame(animate);
-        sphere.rotation.y += 0.0075;
-        sphere.rotation.x += 0.0075;
-        sphere.rotation.z += 0.0075;
-        controls.update();
-        renderer.render(scene, camera);
-    }
-
-    animate();
-}
-
-
 
 export const Greeting = () => {
     const [isShowNeon, setShowNeon] = useState(false);
-    
-    useEffect(() => {
-        // setTimeout(() => {
-        //     setShowNeon(!isShowNeon)
-        // }, 500);
 
-        runSphere();
+    useEffect(() => {
+        setTimeout(() => {
+            setShowNeon(!isShowNeon);
+        }, 500);
     }, [ isShowNeon ]);
-    
+
     return <div className={`relative h-screen bg-section_first bg-no-repeat bg-cover`}>
         <div className={`absolute bg-section_first_neon bg-no-repeat bg-cover ${ isShowNeon ? 'w-full h-screen' : ''}`}></div>
 
-        <div className={"h-screen pb-5 flex flex-col justify-end bg-transparent items-center"}>
-            <h1 className={"mb-5 text-6xl text-white"}>Hi, I'm Ratmir</h1>
-            <canvas id="sphere" className={"absolute top-0 right-0 !w-[400px] !h-[200px] opacity-40"}></canvas>
+        <div className={"h-screen pb-3 flex flex-col justify-end bg-transparent items-center"}>
+            <h1 className={"relative font-network text-6xl text-white overflow-hidden"}>
+                Hi, I'm Ratmir
+            </h1>
+
+            <p className={'pb-5 font-unbounded text-xl text-cyan-200'}>Scroll down & join the portfolio</p>
+            
+            <div className={`mt-2`}>
+                <div className={`
+                    w-[30px] h-[50px] bg-transparent border-solid border-2 border-white rounded-[22px]
+                    flex flex-col justify-center items-center overflow-hidden
+                `}>
+                    <div className={`relative top-0 animate-scroll`}>
+                        <div className={`
+                            w-[10px]
+                            h-[10px]
+                            bg-transparent
+                            border-r-2
+                            border-l-cyan-100
+                            border-b-2
+                            rotate-45
+                            border-t-cyan-100
+                        `}></div>
+
+                        <div className={`
+                            w-[10px]
+                            h-[10px]
+                            bg-transparent
+                            border-r-2
+                            border-l-cyan-100
+                            border-b-2
+                            rotate-45
+                            border-t-cyan-100
+                        `}></div>
+
+                        <div className={`
+                            w-[10px]
+                            h-[10px]
+                            bg-transparent
+                            border-r-2
+                            border-l-cyan-100
+                            border-b-2
+                            rotate-45
+                            border-t-cyan-100
+                        `}></div>
+                    </div>
+                </div>
+
+                <div className={'m-auto mt-2 w-[8px] h-[8px] bg-cyan-200 rounded-[22px] opacity-50'}></div>
+            </div>
         </div>
     </div>
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,28 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      keyframes: {
+        'scroll': {
+          'from': { top: '-100%' },
+          'to': { top: '100%' },
+        }
+      },
+
+      animation: {
+        'scroll': 'scroll 2s linear infinite'
+      },
+
+      colors: {
+        scroll_border: '#ac91b0 '
+      },
+    },
+
+    fontFamily: {
+      'network': 'Network',
+      'unbounded': 'Unbounded'
+    },
+
     backgroundImage: {
       'section_first': "url('~sections/first.jpeg')",
       'section_first_neon': "url('~sections/dac-neon.webp')",
