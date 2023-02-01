192f7d0a962618904dd6e9be1cdf94a0a01885c5
kusainovratmir3@gmail.com <ratmirkusainov4622@gmail.com>
2023-02-01 14:48:59 +0400

    complete bio --feature

---
+++ src/animation/Fog.ts
@@ -0,0 +1,100 @@
+import * as THREE from 'three';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
+import { LuminosityShader } from 'three/examples/jsm/shaders/LuminosityShader';
+import { FilmPass } from 'three/examples/jsm/postprocessing/FilmPass';
+import smoke_texture from '../../public/smoke.png';
+import { runAnimation } from '../utils/runAnimation';
+
+export const runFog = (WebGLCanvas, opacity) => {
+    const scene = new THREE.Scene();
+
+    const camera = new THREE.PerspectiveCamera(95, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.rotation.x = 1.16;
+    camera.rotation.z = 0.27;
+    camera.rotation.y = -0.12;
+    
+    const ambient = new THREE.AmbientLight(0xc8caf7, 0);
+    scene.add(ambient);
+    
+    
+    const directionalLight = new THREE.DirectionalLight(0x7d1bcd);
+    directionalLight.position.set(0, 0, 1);
+    scene.add(directionalLight);
+    
+    
+    
+    
+    const orangeLight = new THREE.PointLight(0xc8caf7, 50, 450, 1.7);
+    orangeLight.position.set(200, 300, 100);
+    
+    const redLight = new THREE.PointLight(0x2323b4, 50, 450, 1.7);
+    redLight.position.set(100, 300, 100);
+    
+    const blueLight = new THREE.PointLight(0xd281f5, 50, 450, 1);
+    blueLight.position.set(300, 300, 200);
+    
+    const purpleLight = new THREE.PointLight(0x480575, 50, 450, 1.7);
+    blueLight.position.set(3, 300, 100);
+    
+    scene.add(orangeLight, redLight, blueLight, purpleLight);
+    
+    
+    
+    
+    const renderer = new THREE.WebGLRenderer({
+        canvas: WebGLCanvas,
+    });
+    
+    renderer.setClearColor(0x000, 0);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    
+    
+    
+    
+    const composer = new EffectComposer(renderer);
+    const renderPass = new RenderPass(scene, camera);
+    const luminosityPass = new ShaderPass(LuminosityShader);
+    const filmPass = new FilmPass(0.35, 0.025, 648, false);
+
+    composer.addPass(filmPass);
+    composer.addPass(renderPass);
+    composer.addPass(luminosityPass);
+    
+    
+    const cloudParticles = [];
+    new THREE.TextureLoader().load(smoke_texture, (texture) => {
+        const cloudGeo = new THREE.PlaneGeometry(500, 500);
+        const cloudMaterial = new THREE.MeshLambertMaterial({ map: texture, transparent: true });
+    
+        for (let cloudOrder = 0; cloudOrder < 50; cloudOrder++) {
+            const cloud = new THREE.Mesh(cloudGeo, cloudMaterial);
+            cloud.position.set(Math.random() * 400, 0, -100);
+            cloud.rotation.x = 1.116;
+            cloud.rotation.y = -0.012;
+            cloud.rotation.z = Math.random() * 2 * Math.PI;
+            cloud.material.opacity = opacity;
+            cloudParticles.push(cloud);
+            scene.add(cloud);
+        }
+    })
+    
+    
+    runAnimation(() => {
+        composer.render(scene, camera);
+        cloudParticles.forEach(p => {
+            p.rotation.z -= 0.0008;
+        })
+    });
+    
+
+    const onWindowResize = () => {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    window.addEventListener("resize", onWindowResize, false);
+}
--- src/components/sections/bio/Biography.tsx
+++ src/components/sections/bio/Biography.tsx
@@ -1,115 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import character from '../../../../public/sections/bio/me.png';
-import cloud from '../../../../public/sections/cloud.svg';
-import * as THREE from 'three';
-import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
-import { GlitchPass } from 'three/examples/jsm/postprocessing/GlitchPass';
-import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
-import { LuminosityShader } from 'three/examples/jsm/shaders/LuminosityShader';
-import { FilmPass } from 'three/examples/jsm/postprocessing/FilmPass';
-import { runAnimation } from '../../../utils/runAnimation';
-import smoke_texture from '../../../../public/smoke.png';
 import { Indicator } from "../../common/Indicator/Indicator";
 import { useTranslation } from "react-i18next";
-import block_menu from '../../../../public/sections/bio/block.svg';
-
-const runFog = () => {
-    const scene = new THREE.Scene();
-
-    const camera = new THREE.PerspectiveCamera(95, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.rotation.x = 1.16;
-    camera.rotation.z = 0.27;
-    camera.rotation.y = -0.12;
-    
-    const ambient = new THREE.AmbientLight(0xc8caf7, 0);
-    scene.add(ambient);
-    
-    
-    const directionalLight = new THREE.DirectionalLight(0x7d1bcd);
-    directionalLight.position.set(0, 0, 1);
-    scene.add(directionalLight);
-    
-    
-    
-    
-    const orangeLight = new THREE.PointLight(0xc8caf7, 50, 450, 1.7);
-    orangeLight.position.set(200, 300, 100);
-    
-    const redLight = new THREE.PointLight(0x2323b4, 50, 450, 1.7);
-    redLight.position.set(100, 300, 100);
-    
-    const blueLight = new THREE.PointLight(0xd281f5, 50, 450, 1);
-    blueLight.position.set(300, 300, 200);
-    
-    const purpleLight = new THREE.PointLight(0x480575, 50, 450, 1.7);
-    blueLight.position.set(3, 300, 100);
-    
-    scene.add(orangeLight, redLight, blueLight, purpleLight);
-    
-    
-    
-    
-    const renderer = new THREE.WebGLRenderer({
-        canvas: document.getElementById('fog'),
-    });
-    
-    renderer.setClearColor(0x000, 0);
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    
-    
-    
-    
-    const composer = new EffectComposer(renderer);
-    const renderPass = new RenderPass(scene, camera);
-    const luminosityPass = new ShaderPass(LuminosityShader);
-    const filmPass = new FilmPass(0.35, 0.025, 648, false);
-
-    composer.addPass(filmPass);
-    composer.addPass(renderPass);
-    composer.addPass(luminosityPass);
-    
-    
-    const cloudParticles = [];
-    new THREE.TextureLoader().load(smoke_texture, (texture) => {
-        const cloudGeo = new THREE.PlaneGeometry(500, 500);
-        const cloudMaterial = new THREE.MeshLambertMaterial({ map: texture, transparent: true });
-    
-        for (let cloudOrder = 0; cloudOrder < 50; cloudOrder++) {
-            const cloud = new THREE.Mesh(cloudGeo, cloudMaterial);
-            cloud.position.set(Math.random() * 400, 0, -100);
-            cloud.rotation.x = 1.116;
-            cloud.rotation.y = -0.012;
-            cloud.rotation.z = Math.random() * 2 * Math.PI;
-            cloud.material.opacity = 0.035
-            cloudParticles.push(cloud);
-            scene.add(cloud);
-        }
-    })
-    
-    
-    runAnimation(() => {
-        composer.render(scene, camera);
-        cloudParticles.forEach(p => {
-            p.rotation.z -= 0.0008;
-        })
-    });
-    
-
-    const onWindowResize = () => {
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-    }
-
-    window.addEventListener("resize", onWindowResize, false);
-}
+import { BiographyFacts } from "./BiographyFacts";
+import { runFog } from "../../../animation/Fog";
 
 
 export const Biography = () => {
     useEffect(() => {
-        runFog();
+        runFog(document.getElementById('fog'), 0.035);
     }, []);
 
     const { t } = useTranslation();
@@ -163,10 +62,8 @@ export const Biography = () => {
             max-lg:rounded-br-[0]
             max-lg:mt-auto
         `}>
-                <div className={`animate-bounce`}>
-                    <Indicator />
-                </div>
-
+                <Indicator />
+            
                 <div className={`
                     p-[10px]
                     max-lg:pl-[30px]
@@ -213,32 +110,7 @@ export const Biography = () => {
                     </p>
 
 
-                    {
-                        [
-                            t('bio.education'),
-                            t('bio.place'),
-                            t('bio.english_level'),
-                            t('bio.russian_level')
-                        ].map(label => <div className={`
-                            mb-5
-                            text-xl
-                          text-slate-200
-                            font-unbounded   
-
-                            max-xl:text-lg
-                            max-s:text-sm
-                        `}>
-                            <span className={`
-                                text-xl
-                              text-cyan-200
-                                underline
-
-                                max-xl:text-lg
-                                max-s:text-base
-                            `}>{label.split(' ')[0]}</span>
-                            {` ${label.split(' ').slice(1).join(' ')}`}
-                        </div>)
-                    }
+                    <BiographyFacts />
                 </div>
         </div>
 
