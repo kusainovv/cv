b1c148dc637e470794d6e88ef16bf48525766ff5
kusainovratmir3@gmail.com <ratmirkusainov4622@gmail.com>
2023-02-07 18:26:48 +0400

    add base layout --feature

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Navigation } from "./components/common/Navigation";
 import { Biography } from "./components/sections/bio/Biography";
 import { Greeting } from "./components/sections/greeting/Greeting";
 import { GreetingBurgerMenu } from "./components/sections/greeting/GreetingBurgerMenu";
+import { OtherProgramming } from "./components/sections/other_programming/OtherProgramming";
 import { Projects } from "./components/sections/projects/Projects";
 import { TechSkills } from "./components/sections/tech_skills/TechSkills";
 import { RootStoreProvider } from "./store/provider/RootStoreProvider";
@@ -13,6 +14,7 @@ export const App = () => {
     <Biography />
     <TechSkills />
     <Projects />
+    <OtherProgramming />
   
     <Navigation />
   
